859e763337190cc548b93d451093084ae318afdb
Yeongmin_Chae <codudals98@naver.com>
2023-08-16 22:07:11 +0900

    chapter 3 : About LinkedList second upload (Various Method of LinkedList) (Second upload)

--- LinkedList.py
+++ LinkedList.py
@@ -115,3 +115,417 @@ class LinkedList_New:
 
 my_linked_list = LinkedList_New(4)
 # And by writing this line, the my_link_list you are creating you have created a head and a tail, it points to the new node, and it assigns the value of 4 to that new node. And gives the linked list a length of 1.
+
+# Lecture5
+# linked list : Print List
+
+# In order to look at all of the values in a linkedList, i've created a method called print list.
+
+
+def print_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
+
+# Lecture6
+# linked list : Append
+
+# We'll have that last item that was in the list point to be the new node will have tail point to the new node that adds into the list.
+# So twp things we did here, last itme and tail pointing to the new node.
+# But we do have an edge case for when we don't have any items in the linked list.
+# Obviously we can't have the last item and tail point to the new node bc there is no last item.
+# In this situation, we r going to have head and tail point to the new node.
+
+
+def append(self, value):
+    # The value that we pass the append method, and then the node class will create our node.
+    new_node = Node(value)
+    if self.head is None:  # We have to determine is the list empty?
+        self.head = new_node
+        self.tail = new_node
+        # Then, head and tail point to the new node.
+    else:  # the situation where there are items already existing in the link list.
+        self.tail.next = new_node
+        self.tail = new_node
+    # The only thing left todo is to increase the length of the link list by one.
+    self.length += 1
+    # optional : in few cases, we'll going to write another method that requires boolean value.
+    return True
+
+# Lecture7
+# linked list : Pop Intro
+
+# In the normal linkedlist, we are going to remove that last item, and we're going to return it, we have 2 edges case.
+
+# Code challenge
+# First edge case is if we have an empty link list.
+# Other edge case is if we only have one node in that list.
+
+# The thing that makes this more complicated us when you move tail to the left like this and remove that the tail is moving the opposite direction of the arrow in the length list.
+# The arrows are all pointing to the right and tail is moving to the left.
+# So i'm going to bring in two variables pre and temp, and i'm going to set them equal to head.
+# And i'm going to remove head and tail, and i'm going to move pre underneath the element.
+# So what we're going todo is we're going to say if temp.next is not none, in other words, we're not to the end of the list.
+# What we're going todo is we're going to set pre equal to temp, which it is already in the first iteration and then we're going to set temp equl to temp.next.
+# And keep doing this, then this will be the last time that it's not none, we're going to move pre up. temp over and now temp.next is equal to none. And these stop right here.
+# So we're going to need that temp variable to return that node that we're popping off the end.
+# But for right now i'm going to remove temp and we'll work with the pre variable first.
+# The only thing the pre variable is four is we're going to set tail equal to pre and that's the only thing we're going to use four.
+# That's going to move tail back. Then we'll say tail.next is none that breaks this off the end of the list.
+# And remember temp is stil pointing four, and we need that to be able to return this. We'll say return temp and that returns this node.(previous element in the linked list.)
+# (4 is the last element in the example linked list.)
+
+# Lecture8
+# linked list : Pop Code.
+# We had three difference scenarios to code for, and i tend to start with the one where we don't have any items and three are a few ways to test for this.
+
+
+def pop(self):
+    if self.length == 0:  # You Could say if head is none, tail is none.
+        return None
+
+# So the next situation we're going to code for is if we have two or more items in the linked list.
+# We're going to have two variables that are going to be temp and pre.
+# We'll say temp equals self.head and pre eqauls self.head.
+# And we're saying while temp,while temp connects is true, if it's pointing to a node it's true.
+# And where this is going to become not true is at the end of the list where that pointer is going to be None. So there are couple of different ways you could write this.
+
+    temp = self.head
+    pre = self.head
+    # In this case, temp.next is pointing at a node, which is going to make this true.
+    while (temp.next):
+        pre = temp  # It is already pointing to the same node as temp.
+        # We'll say temp equals temp.next, which moves temp over like that.
+        temp = temp.next
+    self.tail = pre
+    # we need to take that four and break it off from the end of the linkedlist.
+    self.tail.next = None
+
+# So this is we wrote so far.
+
+
+def pop(self):
+    if self.length == 0:
+        return None
+    temp = self.head
+    pre = self.head
+    while (temp.next):
+        pre = temp
+        temp = temp.next
+    self.tail = pre
+    self.tail.next = None
+    self.length -= 1
+
+# But it still leaves us with one other edge case. That's the situation where you have one node.
+# Let's walk through this situation, we have head and tail pointing to it. We have temp and pre set equal to head.
+# All four of these pointers are going to be pointing at this one node.
+# So the two highlighted lines of code there, temp and pre, i'm going to remove those two and everything above it and we'll walk through these now.
+    while (temp.next):
+        pre = temp
+        self.tail = pre
+        self.tial.next = None
+        self.length -= 1
+# so temp.next is not none we're going to run this loop.
+# Right after that this equates to false, and the while loop does not run.
+# So remove the while loop.
+# And that's all we're going to use temp for. so drop temp out.
+    # They are already pointing at the same item, so that doesn't do anything.
+    self.tail = pre
+    # Tail.next is already none, so that doesn't do anything.
+    self.tial.next = None
+    # The only thing left todo is decrement the length by one. So the length was one bc we had the one node in there and we decrement it to 0.
+    self.length -= 1
+# So When we run this lines of code, We decrement the length to 0. But the problem is head and tail are still pointing at that node. So we have to correct that.
+# Bring that code back here and we'll have to add a few lines of code.
+
+
+def pop(self):
+    if self.length == 0:
+        return None
+    temp = self.head
+    pre = self.head
+    while (temp.next):
+        pre = temp
+        temp = temp.next
+    self.tail = pre
+    self.tail.next = None
+    self.length -= 1
+    if self.length == 0:  # We had to started with one, we decrement it by one and then we got to 0. This just added one is when we started with 1 item.
+        self.head = None
+        self.tail = None
+    return temp  # Which returns the node that we just removed.
+
+
+my_linked_list = my_linked_list(1)
+my_linked_list.append(2)
+
+# my_linked_list.print_list()
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop())
+# (1) Item - Returns 1 Node
+print(my_linked_list.pop())
+# (0) Items - Returns None
+print(my_linked_list.pop())
+
+# Lecture9
+# linked list : Prepend
+# We're going todo is add an item to the beginning of the linked list. So first we want that new node to point at that first node (in linkedlist).
+# So we need to set the next pointer on that node equal to smth that is pointing at that node.
+# So we'll set the next node on the new node equal to head, and that points that over there.
+# And then we'll have head point to the new node. And adds that into list.
+# And of course, this is going to be a little different if we have an empty list like this.
+# When we add it in this situation, we're going to have head and tail point to the new node.
+
+
+def prepend(self, value):
+    # The first thing we're going todo is create the node with that value.
+    new_node = Node(value)
+    # case 1 : empty one
+    # In this situation, we're going to have head and tail point to the new node.
+    # So in order to do this, we have to test to see is the list empty that we're interesting into?
+    if self.length == 0:
+        self.head = new_node
+        self.tail = new_node
+    else:  # We actually have items in the list.
+        # We want that new node to point at that first element of the linkedlist node, and we'll set it equal to the head.
+        new_node.next = self.head
+        self.head = new_node  # Head equals new node.
+    self.length += 1
+    return True
+
+
+my_linked_list = LinkedList_New(2)
+my_linked_list.append(3)
+
+my_linked_list.prepend(1)
+
+my_linked_list.print_list()
+
+# Lecture10
+# linked list : Pop First
+# Case 1 : if we have one itme in the link list
+# This is similar to when we poppend an item off of the other end that we have an edge case for when we have one item and then we have another edge case for when we don't have any items in the link list.
+
+
+def pop_first(self):
+    if self.length == 0:
+        return None
+    # We are removing that first node there. In order to return that, we have to have smth pointing at it.
+    # And we'll call that temp
+    temp = self.head
+    # So we'll set that equal to head.next and then it'll move that over.
+    self.head = self.head.next
+    temp.next = None
+    self.length -= 1
+    # So we'll say if the length is 0, we will have tail be set equal to none, and that solves the problem.
+
+# the entire codes.
+
+
+def pop_first(self):
+    if self.length == 0:
+        return None
+    temp = self.head
+    self.head = self.head.next
+    temp.next = None
+    self.length -= 1
+    if self.length == 0:
+        self.tail = None
+    return temp  # Which is the item that we removed from the linked list.
+
+
+my_linked_list = LinkedList_New(2)
+my_linked_list.append(1)
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop_first())
+# (1) Item - Returns 1 Node
+print(my_linked_list.pop_first())
+# (0) Items - Returns None
+print(my_linked_list.pop_first())
+
+# Lecture11
+# linked list : Get
+
+
+def get(self, index):
+    if index < 0 or index >= self.length:
+        return None
+    # So whatever the index is, that's going to be the number of times we have to move temp over.
+    temp = self.head
+    # So then, we'll create a for loop that is going to run, and get us the correct node.
+    for _ in range(index):
+        temp = temp.next  # That will move temp along the linked list.
+    return temp
+
+
+my_linked_list = LinkedList_New(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+print(my_linked_list.get(2))
+
+# Lecture11
+# linked list : Set
+
+
+# set is a key word in python, so i can't use that as a method name.
+def set_values(self, index, value):
+    # We're going to have a variable that will iterate through to a particular index and that will change the value.
+    # pass the get method of the index and then have temp point to the appropriate node.
+    temp = self.get(index)
+    # Get is going to return one of two things. It is either going to return none if it's an invalid index. Or it's goint to return a node.
+    # So the first thing we have todo is test to see is this pointing to a node, Or is temp equal to none.
+    if temp:
+        temp.value = value  # This is the value that we pass the method.
+        return True
+    # If get method returns none, it's an invalid index, then we'll return false.
+    return False
+
+
+my_linked_list = LinkedList_New(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+my_linked_list.set_value(1, 4)
+
+my_linked_list.print_list()
+
+# Lecture12
+# linked list : Insert
+
+
+def insert(self, index, value):
+    if index < 0 or index > self.length:
+        return False
+    if index == 0:
+        return self.prepend(value)
+    # It has the return keyword in here, which is saying once this prepend method runs, we're not going to keep running code in this method.
+    # The other one is 'self' keyword, we are running this on this particular instance of the link list.
+    if index == self.length:
+        return self.append(value)
+    # In order to add the value middle of the index, we're going to need to have a variable that points to the node before the index where we're inserting this.
+    # The reason why you have it point here instead of the index where You are going to need the next pointer from that node which is you r pointing now.
+    new_node = Node(value)
+    temp = self.get(index - 1)
+    new_node.next = temp.next
+    temp.next = new_node
+    self.length += 1
+    return True
+
+
+my_linked_list = LinkedList_New(0)
+my_linked_list.append(2)
+
+my_linked_list.insert(1, 1)
+
+my_linked_list.print_list()
+
+# Lecture13
+# linked list : Remove
+
+
+def remove(self, index):
+    if index < 0 or index >= self.length:
+        return None
+    # The reason why we return false in other methods(like insert), and return None here?
+    # Bc if we're successful in the insert method we return true, and if we weren't then we return false.
+    # In the other hand, the remove method if we're successful, we're going to return a node.
+    # So we return None here, bc none is the opposite of returning a node.
+    if index == 0:
+        return self.pop_first
+    if index == self.length - 1:
+        return self.pop()
+    # So we've accounted for situations when the index is out of range or if it's the first item or the last item.
+    # So let's look at removing an item that's somewhere in the middle. We'll do the index of two.
+    # At the end of this, we're going to return temp, but we'll need another variable to point to the itme before it.
+    # And to show why we need that variable pointing at the node before the one we're going to remove.
+    # We need that there to be able to say prev.next points to that next node. So you have to have smth to the left of that arrow.
+    prev = self.get(index - 1)
+    # We have a variable at the node before this. This is more efficient and therefore correct way of doing that.
+    temp = prev.next
+    prev.next = temp.next
+    temp.next = None
+    self.length -= 1
+    return temp
+
+# Entire method of remove
+
+
+def remove(self, index):
+    if index < 0 or index >= self.length:
+        return None
+    if index == 0:
+        return self.pop_first()
+    if index == self.length - 1:
+        return self.pop()
+    prev = self.get(index - 1)
+    temp = prev.next
+    prev.next = temp.next
+    temp.next = None
+    self.length -= 1
+    return temp
+
+
+my_linked_list = LinkedList_New(11)
+my_linked_list.append(3)
+my_linked_list.append(23)
+my_linked_list.append(7)
+
+print(my_linked_list.remove(2), '\n')
+
+my_linked_list.print_list()
+
+# Lecture14
+# linked list : Reverse
+
+# This is a very common interview question.
+
+
+def reverse(self):
+
+    # Switch the possesions of the head and tail.
+    temp = self.head
+    self.head = self.tail
+    self.tail = temp
+    after = temp.next  # Variable : Of right side of the temp.
+    before = None  # Variable : Of left side of the temp.
+    # And these 3 variables (temp, after, before) are iterate through the link list as we are reversing everything.
+    for _ in range(self.length):
+        after = temp.next
+        temp.next = before  # This is what flips that arrow over the other way.
+        # After this, there is a gap between elements of the linked list.
+        # So you have to make sure that your linked list never breaks in such a way that you can't get to smth across that gap.
+        # That is what the after variable is, therefore.
+        before = temp
+        temp = after
+
+    # The for loop that goes through the length of the linked list.
+
+# Entire code of the reverse method.
+
+
+def reverse(self):
+    temp = self.head
+    self.head = self.tail
+    self.tail = temp
+    after = temp.next
+    before = None
+    for _ in range(self.length):
+        after = temp.next
+        temp.next = before
+        before = temp
+        temp = after
+
+
+my_linked_list = LinkedList_New(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+my_linked_list.reverse()
+
+my_linked_list.print_list()
